0d7d4ff275c15635e673ef84455a9ac273c4ceb1
Valencia01 <kvalencia.99440@gmail.com>
2023-04-27 22:47:23 -0700

    Added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
 var btn = document.querySelector('.search-btn')
 var userInput = document.querySelector('.city-input')
 
+var cityName = document.querySelector('result-text')
+
 var temp = document.querySelector('.temp');
 var wind = document.querySelector('.wind');
 var humidity = document.querySelector('.humidity');
 
 btn.addEventListener('click',function(){
 
+//This fetches the geo location of the user input giving the long and lat aswell as the name
 fetch('http://api.openweathermap.org/geo/1.0/direct?q='+userInput.value+'&limit=5&appid=736fdd6c798518de531d91856a4b6127')
     .then(response => response.json())
     .then(data => console.log(data))
 
-    .catch(err => alert("try again"))
 
 })
